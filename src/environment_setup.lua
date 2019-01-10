27bcfacb8208ac67d7fd497e8d01da53fc131362
Sven Marcus <s.marcus@outlook.de>
2019-01-10 01:12:22 +0100

    First working test

--- src/environment_setup.lua
+++ src/environment_setup.lua
@@ -1,5 +1,7 @@
 local eal = require "eaw-abstraction-layer"
 local environment = eal.environment
 
+environment.register_busted_matchers()
+
 local mod_path = require "icw_path"
 environment.init(mod_path)
